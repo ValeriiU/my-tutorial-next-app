edfdf43dff887caea0c0e1d31da3401bc43ea823
Valerii Viun <v2455521@ukr.net>
2023-12-30 18:20:12 +0200

    summary page navigation

---
+++ app/ui/probaboard/HeaderPage/HeaderPage.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+const HeaderPage = () => {
+ 
+
+  return (
+    <div className="flex h-[48px] grow items-center justify-between gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium  md:p-2 md:px-3">
+      <div className="flex h-[48px] grow items-center justify-between gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100  hover:text-blue-600  md:p-2 md:px-3">
+      <Link href="#">Header Page</Link></div>
+
+      <div className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+        <Link href='#'>Cosmetics brands</Link> </div>
+
+      <div className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+        <Link href='#'>Contacts</Link> </div>
+
+        <ul className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <li>
+            <Link className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3" href='#'>
+
+            </Link>
+          </li>
+          <li>
+            <Link className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3" href='#'>
+            </Link>
+          </li>
+          <li>
+            <Link className="" href='#'>
+            </Link>
+          </li>
+        </ul>
+      </div>
+  
+  );
+};
+
+export default HeaderPage;
