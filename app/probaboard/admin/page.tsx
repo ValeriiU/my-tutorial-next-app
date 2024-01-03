9bddc0be9e9cbdcf4554b22732d1af7a3b079bfa
Tatiana Kashcha <tkashcha@gmail.com>
2024-01-04 01:15:54 +0200

    Update page.tsx

--- app/probaboard/admin/page.tsx
+++ app/probaboard/admin/page.tsx
@@ -16,8 +16,8 @@ export const metadata = {
 };
 
 async function AdminPage() {
-  const { resources: sneakers } = await cloudinary.api.resources_by_tag(
-    "nextjs-server-actions-upload-sneakers",
+  const { resources: images } = await cloudinary.api.resources_by_tag(
+    "nextjs-server-actions-upload-images",
     { context: true }
   );
 
@@ -30,7 +30,7 @@ async function AdminPage() {
       cloudinary.uploader
         .upload_stream(
           {
-            tags: ["nextjs-server-actions-upload-sneakers"],
+            tags: ["nextjs-server-actions-upload-images"],
           },
           function (error, result) {
             if (error) {
@@ -49,55 +49,54 @@ async function AdminPage() {
     <>
       <p>Admin Page</p>
 
-      <div className="grid grid-rows-[auto_1fr] h-screen">
-        <h2 className="text-xl font-bold mb-4">Add a New Image</h2>
-        <form
-          action={create}
-          className="bg-white border border-slate-200 dark:border-slate-500 rounded p-6 mb-6"
-        >
-          <p className="mb-6">
-            <label htmlFor="image" className="block font-semibold text-sm mb-2">
-              Select an Image to Upload
-            </label>
-            <input
-              id="image"
-              className="block w-full border-slate-400 rounded focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              type="file"
-              name="image"
-              required
-            />
-          </p>
+      <h2 className="text-xl font-bold mb-4">Add a New Image</h2>
+      <form
+        action={create}
+        className="bg-white border border-slate-200 dark:border-slate-500 rounded p-6 mb-6"
+      >
+        <p className="mb-6">
+          <label htmlFor="image" className="block font-semibold text-sm mb-2">
+            Select an Image to Upload
+          </label>
+          <input
+            id="image"
+            className="block w-full border-slate-400 rounded focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            type="file"
+            name="image"
+            required
+          />
+        </p>
 
-          <Button>Submit</Button>
-        </form>
-        <h2 className="text-xl font-bold mb-4">Images</h2>
-        <ul className="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-12">
-          {sneakers.map((sneaker: CloudinaryResource) => {
-            return (
-              <li
-                key={sneaker.public_id}
-                className="rounded overflow-hidden bg-white dark:bg-slate-700"
-              >
-                <div className="relative">
-                  <CldImage
-                    width={400}
-                    height={300}
-                    src={sneaker.public_id}
-                    alt={sneaker.context?.alt || ""}
-                  />
+        <Button>Submit</Button>
+      </form>
+      <h2 className="text-xl font-bold mb-4">Images</h2>
+      <ul className="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-12">
+        {images.map((image: CloudinaryResource) => {
+          return (
+            <li
+              key={image.public_id}
+              className="rounded overflow-hidden bg-white dark:bg-slate-700"
+            >
+              <div className="relative">
+                <CldImage
+                  width={400}
+                  height={300}
+                  src={image.public_id}
+                  alt={image.context?.alt || ""}
+                />
+              </div>
+
+              {image.context?.caption && (
+                <div className="py-4 px-5">
+                  <p className="mb-1 text-md font-bold leading-tight text-neutral-800 dark:text-neutral-50">
+                    {image.context?.caption || ""}
+                  </p>
                 </div>
-                {sneaker.context?.caption && (
-                  <div className="py-4 px-5">
-                    <p className="mb-1 text-md font-bold leading-tight text-neutral-800 dark:text-neutral-50">
-                      {sneaker.context?.caption || ""}
-                    </p>
-                  </div>
-                )}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+              )}
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
