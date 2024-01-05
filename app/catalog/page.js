927f2cb6aba153791a1ec65e429f8e21407eb22d
Tatiana Kashcha <tkashcha@gmail.com>
2024-01-05 19:24:53 +0200

    upd  navigate

--- app/catalog/page.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Catalog",
-  description: "My Catalog Page",
-};
-
-export default function CatalogPage() {
-  return <p>Catalog Page</p>;
-}
--- app/favorite/page.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Favorite",
-  description: "My Favorite Page",
-};
-
-export default function FavoritePage() {
-  return <p>Favorite Page</p>;
-}
