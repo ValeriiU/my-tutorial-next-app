019a54715cad1f8fcb13070a04acbc20c8ba004e
Tatiana Kashcha <tkashcha@gmail.com>
2024-01-13 21:27:55 +0200

    upd id

--- app/probaboard/product/[id]/page.jsx
+++ app/probaboard/product/[id]/page.jsx
@@ -1,20 +1,35 @@
-'use client'
-import * as React from 'react';
-import dataJson from '../../../lib/dataJson'
-import { useRouter } from 'next/navigation';
+// "use client";
+// import * as React from "react";
+import dataJson from "../../../lib/dataJson";
+// import { useRouter } from "next/navigation";
 
-export default function Page() {
-  const { query } = useRouter();
-  const id = { query }
-  if (!id) {
-    return (`Product not ID`)
+export default function Page({ params }) {
+  // const { query } = useRouter();
+
+  // const id = { query };
+
+  // if (!id) {
+  //   return `Product not ID`;
+  // }
+
+  // const id = params.id;
+  // const productById = dataJson.products.find((item) => item.id === Number(id));
+
+  const id = Number(params.id);
+  const productById = dataJson.products.find((item) => item.id === id);
+
+  if (typeof id === "number") {
+    console.log('Змінна id має тип "number".');
+  } else {
+    console.log("Змінна id не є числовою.");
   }
-  dataJson.products.find(item => item.id === id);
+
+  console.log(productById);
+  console.log(id);
 
   return (
     <div>
-      <p>product.id</p>
-        
-     </div>
-
-    )}
+      <p>Product Id Page {id}</p>
+    </div>
+  );
+}
--- app/ui/probaboard/Item-card.jsx
+++ app/ui/probaboard/Item-card.jsx
@@ -2,32 +2,28 @@
 import Image from "next/image";
 // import clsx from "clsx";
 import Link from "next/link";
-import dataJson from  "../../lib/dataJson"
+import dataJson from "../../lib/dataJson";
 import Card from "./card";
 
-
-
 export default async function ItemCards() {
-    'use server'
-    // const query = searchParams?.query || '';
-    // const currentPage = Number(searchParams?.page) || 1;
-  
+  "use server";
+  // const query = searchParams?.query || '';
+  // const currentPage = Number(searchParams?.page) || 1;
+
   // const CardsCatalog = await fetchCatalog();
   return (
-    <div >
+    <div>
       <h2 className="text-lg "> Item Cards </h2>
       <div className="  relative list-none grid-5 grid gap p-0 mb-0 justify-around">
         {/* {CardsCatalog.map((fetchCatalog, i) => { */}
-
-        {dataJson.products.map((product) => (
-          <Card
-          product={product}
-          key={product.id}>
-          </Card>
-        ) )}
-
+        <ul>
+          {dataJson.products.map((product) => (
+            <li key={product.id}>
+              <Card product={product} />
+            </li>
+          ))}
+        </ul>
       </div>
-
     </div>
   );
 }
